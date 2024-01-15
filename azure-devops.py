5f958b50939a7b840a347f4c90b7c93ea16a392b
AkashBalani <balani.a@husky.neu.edu>
2024-01-15 16:04:06 -0500

    Add Azure Pipelines configuration

--- azure-devops.py
+++ azure-devops.py
@@ -47,7 +47,7 @@ os.system("git commit -m \"Add Azure Pipelines configuration\"")
 os.system("git push origin main")
 
 # Configure GitHub Repository Secrets
-os.system(f"gh secret set AZURE_DEVOPS_PAT -b {github_repo_owner}/{github_repo_name} -r {github_repo_owner}/{github_repo_name} -v {azure_devops_pat}")
-os.system(f"gh secret set GITHUB_TOKEN -b {github_repo_owner}/{github_repo_name} -r {github_repo_owner}/{github_repo_name} -v {github_token}")
+os.system(f"gh secret set AZURE_DEVOPS_PAT -b {azure_devops_pat} -r {github_repo_owner}/{github_repo_name} -v selected")
+os.system(f"gh secret set GITHUB_TOKEN -b {github_token} -r {github_repo_owner}/{github_repo_name} -v selected")
 
 
